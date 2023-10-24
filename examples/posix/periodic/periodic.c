8d3a5199e34ef557b0f0cad4f00d79a7b0a54373
Amirhossein Rahmanzadeh <rahmanzadeh.99@gmail.com>
2023-10-24 17:41:13 +0200

    exercise 1

--- examples/posix/periodic/periodic.c
+++ examples/posix/periodic/periodic.c
@@ -1,27 +1,37 @@
-#include <stdio.h>
 #include "tpl_os.h"
+#include <stdio.h>
 
 int main(void)
 {
-    StartOS(OSDEFAULTAPPMODE);
-    return 0;
+  StartOS(OSDEFAULTAPPMODE);
+  return 0;
 }
 
-DeclareAlarm(one_second);
+DeclareAlarm(task_A_timer);
 
-TASK(my_periodic_task)
+TASK(task_A)
 {
-  static int occurence = 0;
-    
-  occurence++;
-  printf("Activation #%d\r\n",occurence);
-    
+  static int task_A_counter = 0;
+
+  printf("Activation task A #%d\r\n", task_A_counter);
+  task_A_counter += 500;
+
+  TerminateTask();
+}
+
+TASK(task_B)
+{
+  static int task_B_counter = 1500;
+
+  printf("Activation task B #%d\r\n", task_B_counter);
+  task_B_counter += 750;
+
   TerminateTask();
 }
 
 TASK(stop)
 {
-  CancelAlarm(one_second);
+  CancelAlarm(task_A_timer);
   printf("Shutdown\r\n");
   ShutdownOS(E_OK);
   TerminateTask();
